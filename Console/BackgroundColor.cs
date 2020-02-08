477e0e8a9ed678f8aca8e8d52b7fdc454390014a
tg-develop <59343164+tg-develop@users.noreply.github.com>
2020-02-08 21:30:43 +0100

    Create BackgroundColor.cs

---
+++ Console/BackgroundColor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Application1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.Clear();
+
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.WriteLine("Hello World!");         
+            Console.Read();
+        }
+    }
+}
