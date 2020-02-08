fc22c0ed18997bed32f11ab2789e73b534960874
tg-develop <59343164+tg-develop@users.noreply.github.com>
2020-02-08 22:52:16 +0100

    Create Beep.cs

---
+++ Console/Beep.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Application1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Some calculation...");
+
+            Console.Beep();
+            Console.Beep();
+            Console.Beep();
+
+            Console.Read();
+        }
+    }
+}
