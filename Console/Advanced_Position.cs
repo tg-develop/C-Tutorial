ef0333a84dddf3d3d09b51893e2bf902d4304343
tg-develop <59343164+tg-develop@users.noreply.github.com>
2020-02-08 23:54:22 +0100

    Create Advanced_Position.cs

---
+++ Console/Advanced_Position.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Application1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            centerText("Hello");
+            centerText("What's up");
+            Console.Read();
+        }
+
+        static void centerText(string text)
+        {
+            Console.CursorLeft = Console.BufferWidth / 2 - text.Length / 2;
+            Console.WriteLine(text);
+        }
+    }
+}
