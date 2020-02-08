1f5babb7c3080475dc32c506f82b7f151cd3b6e0
tg-develop <59343164+tg-develop@users.noreply.github.com>
2020-02-08 22:35:12 +0100

    Create Cursor_Position.cs

---
+++ Console/Cursor_Position.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Application1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.CursorLeft = 3;
+            Console.WriteLine("Row 0 - Column 3");
+
+            Console.CursorTop = 3;
+            Console.WriteLine("Row 2 - Column 1");
+
+            Console.SetCursorPosition(3, 2);
+            Console.WriteLine("Row 3 - Column 3");
+
+            Console.Read();
+        }
+    }
+}
