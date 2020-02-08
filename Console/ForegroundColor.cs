bd761e6181fc45930544ce3c274e9c14bb483008
tg-develop <59343164+tg-develop@users.noreply.github.com>
2020-02-08 21:17:38 +0100

    Create ForegroundColor.cs

---
+++ Console/ForegroundColor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Application1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Hello World!");         
+            Console.Read();
+        }
+    }
+}
