128d1584fddb627797f875da162da053ef3e7442
tg-develop <59343164+tg-develop@users.noreply.github.com>
2020-02-09 22:03:46 +0100

    Create If.cs

---
+++ Conditions/If.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Application1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input your age: ");
+            int age = Int32.Parse(Console.ReadLine());
+
+            if (age > 18)
+            {
+                Console.WriteLine("Age is greater 18!");
+            }
+
+            Console.Read();
+        }
+
+
+    }
+}
