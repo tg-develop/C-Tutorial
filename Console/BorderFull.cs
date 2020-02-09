f2c1b6a098b3fdc2a64baab0719e744f41f4dcca
tg-develop <59343164+tg-develop@users.noreply.github.com>
2020-02-09 12:47:56 +0100

    Exercises Border and Square

---
+++ Console/BorderFull.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Application1
+{
+    class Program
+    {
+        const string cornerLeftTop = "┌";
+        const string cornerLeftBottom = "└";
+        const string cornorRightTop = "┐";
+        const string cornorRightBottom = "┘";
+        const string borderHorizontal = "─";
+        const string borderVertical = "│";
+
+        static void Main(string[] args)
+        {
+            createBorder();
+            Console.Read();
+        }
+
+        static void createBorder()
+        {
+            writeAtPosition(cornerLeftTop, 0, 0);
+            writeAtPosition(cornorRightTop, Console.WindowWidth - 1, 0);
+
+            for (int i = 1; i < Console.WindowWidth - 1; i++)
+            {
+                writeAtPosition(borderHorizontal, i, 0);
+                writeAtPosition(borderHorizontal, i, Console.WindowHeight - 1);
+            }
+
+            writeAtPosition(cornerLeftBottom, 0, Console.WindowHeight - 1);
+            writeAtPosition(cornorRightBottom, Console.WindowWidth - 1, Console.WindowHeight - 1);
+
+
+            for (int i = 1; i < Console.WindowHeight - 1; i++)
+            {
+                writeAtPosition(borderVertical, 0, i);
+                writeAtPosition(borderVertical, Console.WindowWidth - 1, i);
+            }
+
+            Console.SetCursorPosition(0, 0);
+        }
+
+        static void writeAtPosition(string text, int x, int y)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(text);
+        }
+        
+        
+    }
+}
---
+++ Console/Squares.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Application1
+{
+    class Program
+    {
+        const string cornerLeftTop = "┌";
+        const string cornerLeftBottom = "└";
+        const string cornorRightTop = "┐";
+        const string cornorRightBottom = "┘";
+        const string borderHorizontal = "─";
+        const string borderVertical = "│";
+
+        static void Main(string[] args)
+        {
+            createBorder(5, 5, 20, 10);
+            createBorder(10, 20, 10, 5);
+            createBorder(40, 12, 16, 8);
+
+            Console.Read();
+        }
+
+        static void createBorder(int xStart, int yStart, int width, int height)
+        {
+            writeAtPosition(cornerLeftTop, xStart, yStart);
+            writeAtPosition(cornorRightTop, xStart + width - 1, yStart);
+
+            for (int i = 1; i < width - 1; i++)
+            {
+                writeAtPosition(borderHorizontal, xStart + i, yStart);
+                writeAtPosition(borderHorizontal, xStart + i, yStart + height - 1);
+            }
+
+            writeAtPosition(cornerLeftBottom, xStart, yStart + height - 1);
+            writeAtPosition(cornorRightBottom, xStart + width - 1, yStart + height - 1);
+
+
+            for (int i = 1; i < height - 1; i++)
+            {
+                writeAtPosition(borderVertical, xStart, yStart + i);
+                writeAtPosition(borderVertical, xStart + width - 1, yStart + i);
+            }
+
+            Console.SetCursorPosition(0, 0);
+        }
+
+        static void writeAtPosition(string text, int x, int y)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(text);
+        }
+        
+        
+    }
+}
