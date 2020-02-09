08a6a14da6e6e0dea6ba6e47ec8b230499b755d2
tg-develop <59343164+tg-develop@users.noreply.github.com>
2020-02-09 15:12:45 +0100

    Create Input.cs

---
+++ Console/Input.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Application1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Your name is " + name + "!");
+
+            Console.Read();
+        }
+
+
+    }
+}
