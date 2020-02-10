8300a75a1a9268fbbf2f64e90066055f0a5c61e3
tg-develop <59343164+tg-develop@users.noreply.github.com>
2020-02-10 21:20:41 +0100

    Create Group_letters.cs

---
+++ Conditions/Group_letters.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Application1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Input your name: ");
+            string name = Console.ReadLine();
+            char letter = name.ToLower()[0];
+            string group = "";
+
+            if (letter >= 'a' && letter <= 'h')
+            {
+                group = "A";
+            }
+            else if (letter >= 'i' && letter <= 'o')
+            {
+                group = "B";
+            }
+            else if (letter > 'p' && letter <= 'w')
+            {
+                group = "C";
+            }
+            else if (letter >= 'x' && letter <= 'z')
+            {
+                group = "D";
+            }
+            else
+            {
+                group = "Invalid";
+            }
+
+            Console.WriteLine($"Username: {name} - Group: {group}");
+
+
+            Console.Read();
+        }
+    }
+}
