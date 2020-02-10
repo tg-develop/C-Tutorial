b8869ea7084359f989d9cfc724df281746709c65
tg-develop <59343164+tg-develop@users.noreply.github.com>
2020-02-10 20:34:24 +0100

    Create Multiple_Conditions.cs

---
+++ Conditions/Multiple_Conditions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Application1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Input random number: ");
+            int number = Int32.Parse(Console.ReadLine());
+
+            if (number % 2 == 0 && number % 4 == 0)
+            {
+                Console.WriteLine("Yes! True");
+            }
+            else
+            {
+                Console.WriteLine("No, false!");
+            }
+
+
+            Console.Read();
+        }
+    }
+}
