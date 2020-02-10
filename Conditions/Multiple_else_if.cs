27d9553539aad8451c4e318007f43b5c0216f806
tg-develop <59343164+tg-develop@users.noreply.github.com>
2020-02-10 11:28:46 +0100

    Create Multiple_else_if.cs

---
+++ Conditions/Multiple_else_if.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Application1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input your age: ");
+            int age = Int32.Parse(Console.ReadLine());
+
+            if (age < 18)
+            {
+                Console.WriteLine("Below 18 years old");
+            }
+            else if (age < 31)
+            {
+                Console.WriteLine("Between 18 and 30 years old");
+            }
+            else if (age < 51)
+            {
+                Console.WriteLine("Between 31 and 50 years old");
+            }
+            else
+            {
+                Console.WriteLine("Not Matched!");
+            }
+
+            Console.Read();
+        }
+    }
+}
