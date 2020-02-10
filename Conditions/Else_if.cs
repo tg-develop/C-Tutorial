726238dfa8333df9f9ba35766d4f9b61900bf3e1
tg-develop <59343164+tg-develop@users.noreply.github.com>
2020-02-10 12:19:40 +0100

    Create Else_if.cs

---
+++ Conditions/Else_if.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Application1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input the keyword: ");
+            string key = Console.ReadLine();
+
+            if(key == "K1NG")
+            {
+                Console.WriteLine("Hello leader");
+            }
+            else if (key == "M3MB3R")
+            {
+                Console.WriteLine("Hello member");
+            }
+            
+
+            Console.Read();
+        }
+    }
+}
