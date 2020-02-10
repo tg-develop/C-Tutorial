d300ff78575304edfb7accd2e0656110361079f0
tg-develop <59343164+tg-develop@users.noreply.github.com>
2020-02-10 17:25:46 +0100

    Create Switch_case.cs

---
+++ Conditions/Switch_case.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Application1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+
+                Console.Write("Input status code: ");
+                int code = Int32.Parse(Console.ReadLine());
+
+                switch (code)
+                {
+                    case 1: Console.WriteLine("Ok!"); break;
+                    case 2: Console.WriteLine("Danger!"); break;
+                    case 3: Console.WriteLine("Alarm!"); break;
+                    default: Console.WriteLine("Error!"); break;
+                }
+            }
+
+            Console.Read();
+        }
+    }
+}
