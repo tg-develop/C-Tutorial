c8627e5660fb67df009ad52e0514f27da7db1194
tg-develop <59343164+tg-develop@users.noreply.github.com>
2020-02-10 17:48:49 +0100

    Update Switch_case.cs

--- Conditions/Switch_case.cs
+++ Conditions/Switch_case.cs
@@ -8,20 +8,17 @@ namespace Application1
     {
         static void Main(string[] args)
         {
-            while (true)
+            Console.Write("Input status code: ");
+            int code = Int32.Parse(Console.ReadLine());
+
+            switch (code)
             {
-
-                Console.Write("Input status code: ");
-                int code = Int32.Parse(Console.ReadLine());
-
-                switch (code)
-                {
-                    case 1: Console.WriteLine("Ok!"); break;
-                    case 2: Console.WriteLine("Danger!"); break;
-                    case 3: Console.WriteLine("Alarm!"); break;
-                    default: Console.WriteLine("Error!"); break;
-                }
+                case 1: Console.WriteLine("Ok!"); break;
+                case 2: Console.WriteLine("Danger!"); break;
+                case 3: Console.WriteLine("Alarm!"); break;
+                default: Console.WriteLine("Error!"); break;
             }
+
 
             Console.Read();
         }
