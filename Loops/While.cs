bb53ea034c27b338d2ec0ded7b337947a43db632
tg-develop <59343164+tg-develop@users.noreply.github.com>
2020-02-11 21:03:48 +0100

    Create While.cs

---
+++ Loops/While.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Application1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool input = true;
+            int sum = 0;
+
+            while(input)
+            {
+                Console.Write("Enter number: ");
+                sum += Int32.Parse(Console.ReadLine());
+
+                Console.Write("Enter more number? (y / n): ");
+
+                if(Console.ReadLine() == "n")
+                {
+                    input = false;
+                }
+            }
+
+            Console.WriteLine($"Sum: {sum}");
+
+
+            Console.Read();
+        }
+    }
+}
