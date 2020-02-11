6f173b5c06e61f014c41ea4cb250cf7bfa06995e
tg-develop <59343164+tg-develop@users.noreply.github.com>
2020-02-11 16:14:56 +0100

    Create Discount.cs

---
+++ Conditions/Discount.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Application1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input the price of your purchase: ");
+            double price = Double.Parse(Console.ReadLine());
+            double discout = 0;
+
+            if (price >= 500)
+                discout = 0.4;
+            else if (price >= 200)
+                discout = 0.25;
+            else if (price >= 100)
+                discout = 0.1;
+
+            double newPrice = price * (1.0 - discout);
+
+            Console.WriteLine($"Old Price: {price} - Discount percent: {discout * 100}% - New Price: {newPrice}");
+
+
+            Console.Read();
+        }
+    }
+}
